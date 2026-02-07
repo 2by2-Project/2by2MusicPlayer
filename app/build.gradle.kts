72eb187e89fdb1c47eecbcd55985ca9bbbd97524
kailua <kailua@project2by2.jp>
2026-02-07 09:54:38 +0900

    Bump to 1.1b

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "jp.project2by2.musicplayer"
         minSdk = 24
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.1b"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
